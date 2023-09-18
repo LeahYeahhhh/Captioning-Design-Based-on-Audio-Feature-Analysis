f65793180c4c357fee320eb22d8b65dd4f8bf020
Yiyang Jiang <143955750+LeahYeahhhh@users.noreply.github.com>
2023-09-18 12:21:22 +0100

    Add files via upload

---
+++ Captioning_Design/Captioning/supportive_caption.js
@@ -0,0 +1,521 @@
+
+    let video;
+    let canvas;
+    let subtitles; // 用于存储字幕数据的数组
+    let emphsiseSubtitles;//[低语]
+    let currentSubtitleIndex = -1; // 当前字幕的索引
+
+    let emphsiseSubtitleIndex = -1; // 当前字幕的索引
+    let shoutSubtitles;//样式shout存储字幕的数组【大喊】
+    let shoutSubtitleIndex = -1; // 当前shout字幕的索引
+
+    let whisperSubtitles;//样式whisper存储字幕的数组【悄悄话】
+    let whisperSubtitleIndex = -1; // 当前whisper字幕的索引
+    let myFont; //自定义字体对象对应样式emphsiseSubtitles
+    let currentVideoTime = 0; //存储播放时间
+    let font; //全局字体样式
+    let wordDrawer;//样式 shout
+    //样式2
+    class WordDrawer {
+        constructor(word, x, y, size) {
+            this.word = word;
+            this.points = this.getFontOutlinePointsForWord(word, x, y, size);
+        }
+
+        getFontOutlinePointsForWord(word, x, y, size) {
+            return font.textToPoints(word, x, y, size, { // 使用传递的字体大小
+                sampleFactor: 0.3,
+                simplifyThreshold: 0,
+            });
+        }
+
+
+        draw() {
+            push();
+            noStroke();
+            for (let i = 0; i < this.points.length; i++) {
+                let xOffset = random(-1, 1);
+                let yOffset = random(-1, 1);
+                let randomColor = color(random(0, 255), random(0, 255), random(0, 255));
+                let p = this.points[i];
+                fill(randomColor);
+                circle(p.x + 0, p.y + 0, 3);
+            }
+            pop();
+        }
+    }
+
+    class CustomFont {
+        constructor(text, x, y, textSize) {
+            this.text = text;
+            this.x = x;
+            this.y = y;
+            this.textSize = textSize;
+
+            // 初始化这些变量
+            this.rectWidth = 0;
+            this.rectHeight = 0;
+
+            // 创建一个临时的图形对象，用于计算文本尺寸
+            let tempGraphic = createGraphics(1, 1);
+            tempGraphic.textSize(this.textSize);
+            // tempGraphic.textLeading(this.textSize * 1.5);
+
+            // 计算文本的宽度和高度
+            this.rectWidth = tempGraphic.textWidth(this.text) + 20;
+            this.rectHeight = tempGraphic.textAscent() + tempGraphic.textDescent() + 20;
+
+            // 创建实际图形对象并设置文本属性
+            this.graphic = createGraphics(this.rectWidth, this.rectHeight);
+            this.graphic.fill(255);
+            this.graphic.textSize(this.textSize);
+            // this.graphic.textLeading(this.textSize * 1.5);
+            this.graphic.textAlign(CENTER, CENTER);
+            this.graphic.text(this.text, this.rectWidth / 2, this.rectHeight / 2);
+        }
+
+        display() {
+            let tileSize = map(0, 0, this.rectWidth, 10, 20);
+            for (let i = 0; i < this.rectWidth; i += tileSize) {
+                for (let j = 0; j < this.rectHeight; j += tileSize) {
+                    let mouseFactor = map(0, 0, this.rectWidth, 0.3, 1);
+                    let distortionX = sin(frameCount * 0.05 + (i / tileSize) * 0.15 + j * 0.2) * 5;
+                    let distortionY = sin(frameCount * 0.025 + (i / tileSize) * 0.1 + j * 0.1) * 2.5;
+                    image(
+                        this.graphic,
+                        this.x + i - this.rectWidth / 2, // 调整 X 坐标
+                        this.y + j - this.rectHeight / 2, // 调整 Y 坐标
+                        tileSize,
+                        tileSize,
+                        i + distortionX,
+                        j + distortionY,
+                        tileSize,
+                        tileSize
+                    );
+                }
+            }
+        }
+    }
+
+
+    function parseSRT(subtitleText) {
+        const subtitles = [];
+        const subtitleBlocks = subtitleText.split('\n\n');
+
+        for (const block of subtitleBlocks) {
+            const lines = block.split('\n');
+            if (lines.length >= 3) {
+                const index = parseInt(lines[0]);
+                const timeString = lines[1];
+                const text = lines.slice(2).join('\n');
+
+
+                // 测试函数
+                const startTimeValue = timeString.split(' --> ')[0];
+                const startTime = timeStringToSeconds(startTimeValue);
+
+
+                const endTimeValue = timeString.split(' --> ')[1];
+                const endTime = timeStringToSeconds(endTimeValue);
+
+                subtitles.push({
+                        index,
+                        startTime,
+                        endTime,
+                        text
+                    }
+                );
+            }
+        }
+
+        return subtitles;
+    }
+
+
+    function timeStringToSeconds(timeString) {
+        // 使用正则表达式将时间字符串拆分为小时、分钟、秒和毫秒部分
+        const regex = /(\d{2}):(\d{2}):(\d{2}),(\d{3})/;
+        const match = timeString.match(regex);
+
+        // if (!match) {
+        //     throw new Error("error");
+        // }
+
+        // 从正则匹配中提取小时、分钟、秒和毫秒的值
+        const hours = parseInt(match[1]);
+        const minutes = parseInt(match[2]);
+        const seconds = parseInt(match[3]);
+        const milliseconds = parseInt(match[4]);
+
+        // 计算总秒数，包括毫秒（保留3位小数）
+        const totalSeconds = hours * 3600 + minutes * 60 + seconds + milliseconds / 1000;
+
+        return totalSeconds.toFixed(3); // 保留3位小数
+    }
+
+    function preload() {
+        subtitles = loadStrings('assets/01.srt'); // 替换成您的SRT文件路径
+        emphsiseSubtitles = loadStrings('assets/emphsise.srt'); // 替换成您的SRT文件路径
+        shoutSubtitles = loadStrings('assets/shout.srt'); // 替换成您的SRT文件路径
+        whisperSubtitles = loadStrings('assets/whisper.srt'); // 替换成您的SRT文件路径
+        font = loadFont("assets/arial.ttf");
+    }
+
+
+    function setup() {
+
+
+        canvas = createCanvas(1920, 1080);
+
+        // 解析字幕
+        subtitles = parseSRT(subtitles.join('\n'));
+        emphsiseSubtitles = parseSRT(emphsiseSubtitles.join('\n'));
+        shoutSubtitles = parseSRT(shoutSubtitles.join('\n'));
+        whisperSubtitles = parseSRT(whisperSubtitles.join('\n'));
+
+        video = createVideo('assets/01.mp4');
+
+        video.hide();
+        //canvas.mousePressed(startVideo);
+
+        button = createButton('播放');
+        button2 = createButton('暂停')
+        button.position(20, 20);
+        button2.position(80, 20);
+        button.mousePressed(startVideo);
+        button2.mousePressed(pauseVideo);
+        textAlign(LEFT);
+
+    }
+
+
+    currentVideoTime = 0;
+
+
+    function shout(fullString, x, y) {
+        let startX = x
+        if (typeof shoutSubtitles[shoutSubtitleIndex] !== "undefined") {
+        //     ////////////////////////第一行
+            let searchFull = fullString;
+            let searchString = shoutSubtitles[shoutSubtitleIndex].text;
+            let index = searchFull.indexOf(searchString);
+        //
+            if (index !== -1) {
+
+                let foundPart1 = fullString.slice(0, index);
+                let foundPart = searchString;
+                let foundPart2 = fullString.slice(index + searchString.length, fullString.length);
+
+                fill(255);
+                textSize(50);
+
+                let x1 = startX;
+                let x2 = startX + textWidth(foundPart1);
+                let x3 = startX + textWidth(foundPart1) + textWidth(foundPart);
+
+
+
+                // text(foundPart1, x1, y);
+
+                whisper(foundPart1, x1, y);
+
+                wordDrawer = new WordDrawer(foundPart, x2 , y , 50);
+                wordDrawer.draw();
+                // text(foundPart2, x3, y);
+                whisper(foundPart2, x3, y);
+
+                console.log("前-"+foundPart1);
+                console.log("查找-"+foundPart);
+                console.log("后"+foundPart2);
+
+
+            } else {
+                console.log("找不到"+fullString);
+                // text(fullString, x, y);
+                whisper(fullString, x, y);
+
+
+            }
+        //
+        }
+    }
+
+    function w2(fullString, x, y) {
+        let startX = x
+        if (typeof whisperSubtitles[whisperSubtitleIndex] !== "undefined") {
+            //     ////////////////////////第一行
+            let searchFull = fullString;
+            let searchString = whisperSubtitles[whisperSubtitleIndex].text;
+            let index = searchFull.indexOf(searchString);
+            //
+            if (index !== -1) {
+
+                let foundPart1 = fullString.slice(0, index);
+                let foundPart = searchString;
+                let foundPart2 = fullString.slice(index + searchString.length, fullString.length);
+
+                fill(255);
+                textSize(50);
+
+                let x1 = startX;
+                let x2 = startX + textWidth(foundPart1);
+                let x3 = startX + textWidth(foundPart1) + textWidth(foundPart);
+
+
+
+                text(foundPart1, x1, y);
+
+                whisperFont(foundPart, x2 , y , 50);
+
+                text(foundPart2, x3, y);
+                // whisper(foundPart2, x3, y);
+
+                console.log("前-"+foundPart1);
+                console.log("查找-"+foundPart);
+                console.log("后"+foundPart2);
+
+
+            } else {
+                console.log("找不到"+fullString);
+                text(fullString, x, y);
+                // whisper(fullString, x, y);
+
+
+            }
+            //
+        }else{
+            console.log("找不到"+fullString);
+            text(fullString, x, y);
+        }
+    }
+
+
+    function whisperFont(foundPart, x2 , y , size) {
+        push();
+        textSize(size);
+        fill(255,100);
+        text(foundPart, x2 , y);
+        pop();
+    }
+
+    function whisper(fullString, x, y) {
+        let startX = x
+        if (typeof whisperSubtitles[whisperSubtitleIndex] !== "undefined") {
+            //     ////////////////////////第一行
+            let searchFull = fullString;
+            let searchString = whisperSubtitles[whisperSubtitleIndex].text;
+            let index = searchFull.indexOf(searchString);
+            //
+            if (index !== -1) {
+
+                let foundPart1 = fullString.slice(0, index);
+                let foundPart = searchString;
+                let foundPart2 = fullString.slice(index + searchString.length, fullString.length);
+
+                fill(255);
+                textSize(50);
+
+                let x1 = startX;
+                let x2 = startX + textWidth(foundPart1);
+                let x3 = startX + textWidth(foundPart1) + textWidth(foundPart);
+
+
+
+                text(foundPart1, x1, y);
+
+                whisperFont(foundPart, x2 , y , 40);
+
+                // wordDrawer = new WordDrawer(foundPart, x2 , y , 50);
+                // wordDrawer.draw();
+                textSize(50);
+                text(foundPart2, x3, y);
+
+                console.log("前-"+foundPart1);
+                console.log("查找-"+foundPart);
+                console.log("后"+foundPart2);
+
+
+            } else {
+                textSize(50);
+                console.log("找不到"+fullString);
+                text(fullString, x, y);
+
+            }
+            //
+        }else{
+            textSize(50);
+            console.log("找不到"+fullString);
+            text(fullString, x, y);
+        }
+    }
+
+    function wayTwo() {
+        //第二种方案，一直播放，挖空关键字
+        for (let i = 0; i < emphsiseSubtitles.length; i++) {
+
+            if (emphsiseSubtitles[i].startTime < currentVideoTime && currentVideoTime < emphsiseSubtitles[i].endTime) {
+                emphsiseSubtitleIndex = i;
+            }
+
+        }
+
+        for (let i = 0; i < shoutSubtitles.length; i++) {
+
+            if (shoutSubtitles[i].startTime < currentVideoTime && currentVideoTime < shoutSubtitles[i].endTime) {
+
+                shoutSubtitleIndex = i;
+            }
+
+        }
+        for (let i = 0; i < whisperSubtitles.length; i++) {
+
+            if (whisperSubtitles[i].startTime < currentVideoTime && currentVideoTime < whisperSubtitles[i].endTime) {
+
+                whisperSubtitleIndex = i;
+            }
+
+        }
+
+
+        for (let i = 0; i < subtitles.length; i++) {
+            //如果字幕在播放
+            if (currentVideoTime > subtitles[i].startTime && currentVideoTime <= subtitles[i].endTime
+            ) {
+
+                textSize(50);
+                let fullString = subtitles[i].text.split("\n");
+
+                //解析emphsiseSubtitles
+                if (typeof emphsiseSubtitles[emphsiseSubtitleIndex] !== "undefined") {
+                    ////////////////////////第一行
+                    let searchFull = fullString[0];
+                    let searchString = emphsiseSubtitles[emphsiseSubtitleIndex].text;
+                    let index = searchFull.indexOf(searchString);
+
+                    if (index !== -1) {
+
+                        let foundPart1 = fullString[0].slice(0, index);
+                        let foundPart = searchString;
+                        let foundPart2 = fullString[0].slice(index + searchString.length, fullString[0].length);
+
+                        fill(255);
+                        textSize(50);
+                        let startX = width / 2 - textWidth(searchFull) / 2
+                        let x1 = startX;
+                        let x2 = startX + textWidth(foundPart1);
+                        let x3 = startX + textWidth(foundPart1) + textWidth(foundPart);
+
+                        let y = height / 2 + 300;
+                        shout(foundPart1, startX, y);
+                        // text(foundPart1, x1, y);
+                        myFont = new CustomFont(foundPart, x2 + textWidth(foundPart) / 2, y - 10, 50);
+                        myFont.display();
+                        // text(foundPart2, x3, y);
+                        shout(foundPart2, x3, y);
+                        // shout(foundPart2, startX + textWidth(foundPart1) + textWidth(foundPart),  height / 2 + 300);
+
+                    } else {
+                        let startX = width / 2 - textWidth(fullString[0]) / 2;
+                        // text(fullString[0], startX, height / 2 + 300 - 5);
+                        shout(fullString[0], startX, height / 2 + 300 - 5);
+                    }
+
+                    //////////////////////// // 第二行
+
+                    if (typeof fullString[1] !== "undefined") {
+                        let searchFull = fullString[1];
+                        let searchString = emphsiseSubtitles[emphsiseSubtitleIndex].text;
+                        let index = searchFull.indexOf(searchString);
+
+                        if (index !== -1) {
+
+                            let foundPart1 = searchFull.slice(0, index);
+                            let foundPart = searchString;
+                            let foundPart2 = searchFull.slice(index + searchString.length, searchFull.length);
+
+                            fill(255);
+                            textSize(50);
+                            let startX = width / 2 - textWidth(searchFull) / 2
+                            let x1 = startX;
+                            let x2 = startX + textWidth(foundPart1);
+                            let x3 = startX + textWidth(foundPart1) + textWidth(foundPart);
+
+                            let y = height / 2 + 350;
+                            //解析shout
+                            shout(foundPart1, startX, height / 2 + 350);
+                            // text(foundPart1, x1, y);
+                            myFont = new CustomFont(foundPart, x2 + textWidth(foundPart) / 2, y - 10, 50);
+                            myFont.display();
+                            shout(foundPart2, x3, y);
+                            // text(foundPart2, x3, y);
+
+
+                        } else {
+                            let startX = width / 2 - textWidth(searchFull) / 2;
+                            // text(searchFull, startX, height / 2 + 350);
+                            shout(searchFull, startX, height / 2 + 350);
+
+                        }
+                    }
+                } else {
+                    let startX = width / 2 - textWidth(fullString[0]) / 2;
+                    // text(fullString[0], startX, height / 2 + 300 - 5);
+                    shout(fullString[0], startX, height / 2 + 300 - 5);
+                    if (typeof fullString[1] !== "undefined") {
+                        startX = width / 2 - textWidth(fullString[1]) / 2;
+                        // text(fullString[1], startX, height / 2 + 350);
+                        shout(fullString[1], startX, height / 2 + 350);
+                    }
+                }
+
+
+            }
+        }
+
+
+        // 获取视频的当前播放时间
+        currentVideoTime = video.time();
+
+
+        // 显示当前字幕
+        // update2(currentVideoTime);
+    }
+
+    function draw() {
+
+
+        background(0);
+        push();
+        translate(0, -50);
+        image(video, 0, 0, width, height);
+        pop();
+
+        currentVideoTime = video.time();
+
+
+        // wayOne();
+
+
+        wayTwo();
+        // 第二种方案，挖空
+
+    }
+
+    function pauseVideo() {
+        video.pause();
+    }
+
+    function startVideo() {
+
+        video.loop(); // 播放视频
+        let fsElement = document.documentElement; // 获取文档的根元素
+        if (fsElement.requestFullscreen) {
+            fsElement.requestFullscreen(); // 请求全屏
+        } else if (fsElement.mozRequestFullScreen) {
+            fsElement.mozRequestFullScreen(); // Firefox
+        } else if (fsElement.webkitRequestFullscreen) {
+            fsElement.webkitRequestFullscreen(); // Chrome, Safari, and Opera
+        } else if (fsElement.msRequestFullscreen) {
+            fsElement.msRequestFullscreen(); // IE/Edge
+        }
+    }
